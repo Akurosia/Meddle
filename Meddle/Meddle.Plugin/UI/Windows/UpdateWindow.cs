a58bd82081dd402ba3f81eb350fb3fd4bdd1c527
Jaquesy <20432486+PassiveModding@users.noreply.github.com>
2026-07-19 18:29:41 +1000

    add update notes init

--- Meddle/Meddle.Plugin/UI/Windows/UpdateWindow.cs
+++ Meddle/Meddle.Plugin/UI/Windows/UpdateWindow.cs
@@ -231,6 +231,17 @@ public class UpdateWindow : Window
                 new TextUpdateLine(" + Fixes for housing items not being dyed correctly."),
                 new TextUpdateLine(" + Removed validationg for when a partial skeletons ConnectedBoneIndex matched its own index, which caused issues exporting some skeletons")
             ]
+        },
+        new()
+        {
+            Tag = "Grass",
+            Date = "2026-07-19",
+            Changes =
+            [
+                new WarningUpdateLine(" ! NOTE: Update the MeddleTools blender addon to support import of grass blades."),
+                new TextUpdateLine(" + Updated to support exporting grass models (shrubs, bushes etc.)"),
+                new TextUpdateLine(" + Updated to support exporting grass blades as a point cloud"),
+            ]
         }
     ];
     
